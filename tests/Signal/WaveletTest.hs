2e9862b43d9645bfa6db213f38ecb27a77fc2f02
Jan Stolarek <jan.stolarek@p.lodz.pl>
2012-09-23 13:27:13 +0200

    Added HUnit test infrastructure
    Created sample tests along with data providers.

--- tests/Signal/WaveletTest.hs
+++ tests/Signal/WaveletTest.hs
@@ -1,9 +1,30 @@
 module Signal.WaveletTest where
 
 import Signal.Wavelet
+import Test.HUnit
 
 propIdentityShift1 :: [Double] -> Bool
 propIdentityShift1 xs = cyclicShiftLeft (cyclicShiftRight xs) == id xs
 
 propIdentityShift2 :: [Double] -> Bool
 propIdentityShift2 xs = cyclicShiftRight (cyclicShiftLeft xs) == id xs
+
+testCyclicShiftLeft :: ([Double], [Double]) -> Assertion
+testCyclicShiftLeft (input, expected) = 
+    expected @=? cyclicShiftLeft input
+
+testCyclicShiftRight :: ([Double], [Double]) -> Assertion
+testCyclicShiftRight (input, expected) = 
+    expected @=? cyclicShiftRight input
+
+testCyclicShiftLeftDataProvider :: [([Double], [Double])]
+testCyclicShiftLeftDataProvider = 
+    [
+     ( [1,2,3,4], [2,3,4,1] )
+    ]
+
+testCyclicShiftRightDataProvider :: [([Double], [Double])]
+testCyclicShiftRightDataProvider = 
+    [
+     ( [1,2,3,4], [4,1,2,3] )
+    ]
--- tests/Signal/WaveletTestSuite.hs
+++ tests/Signal/WaveletTestSuite.hs
@@ -2,6 +2,8 @@ module Main ( main ) where
 
 import Test.Framework
 import Test.Framework.Providers.QuickCheck2
+import Test.Framework.Providers.HUnit
+import qualified Test.HUnit as HU
 
 import Signal.WaveletTest
 
@@ -13,7 +15,18 @@ tests =
   [ 
     testGroup "Signal shifts" 
     [ 
-      testProperty "L/R shift composition" propIdentityShift1 ,
-      testProperty "R/L shift composition" propIdentityShift2
+      testProperty "L/R shift composition"  propIdentityShift1 ,
+      testProperty "R/L shift composition"  propIdentityShift2 ,
+      testWithProvider "Cyclic shift left"  testCyclicShiftLeft 
+                                            testCyclicShiftLeftDataProvider,
+      testWithProvider "Cyclic shift right" testCyclicShiftRight
+                                            testCyclicShiftRightDataProvider
     ]
   ]
+
+testWithProvider :: String -> (a -> HU.Assertion) -> [a] -> Test
+testWithProvider testGroupName testFunction =
+    testGroup testGroupName . map createTest . zipWith assignName [1 :: Int ..]
+      where 
+        createTest (name, dataSet)   = testCase name $ testFunction dataSet
+        assignName setNumber dataSet = ("Data set " ++ show setNumber, dataSet)
