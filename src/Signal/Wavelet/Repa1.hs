4bbaac58356d8322b617b40b9980746be5f27efb
Jan Stolarek <jan.stolarek@p.lodz.pl>
2012-10-20 22:53:44 +0200

    Removed code repetition by introducing worker

--- src/Signal/Wavelet/Repa1.hs
+++ src/Signal/Wavelet/Repa1.hs
@@ -9,30 +9,30 @@ import Signal.Wavelet.Repa.Common
 
 {-# INLINE dwt #-}
 dwt :: (Source r Double) 
-       => Array r DIM1 Double 
-       -> Array U DIM1 Double 
-       -> Array U DIM1 Double
-dwt !angles !signal = go layers signal
-    where
-      go  1 !sig      = doLayer sig 1
-      go !n !sig      = go (n-1) (forceS . csl $ doLayer sig n)
-      {-# INLINE doLayer #-}
-      doLayer !sig !n = forceP . lattice (weights ! (Z :. (layers - n))) $ sig
-      {-# INLINE weights #-}
-      weights         = a2w angles
-      {-# INLINE layers #-}
-      layers          = size . extent $ angles
+    => Array r DIM1 Double 
+    -> Array U DIM1 Double 
+    -> Array U DIM1 Double
+dwt !angles !signal = dwtWorker csl angles signal
 
-      
+
 {-# INLINE idwt #-}
 idwt :: (Source r Double) 
-        => Array r DIM1 Double 
-        -> Array U DIM1 Double 
-        -> Array U DIM1 Double
-idwt !angles !signal = go layers signal
+     => Array r DIM1 Double 
+     -> Array U DIM1 Double 
+     -> Array U DIM1 Double
+idwt !angles !signal = dwtWorker csr angles signal
+
+
+{-# INLINE dwtWorker #-}
+dwtWorker :: (Source r Double) 
+          => (Array U DIM1 Double -> Array D DIM1 Double)
+          -> Array r DIM1 Double 
+          -> Array U DIM1 Double 
+          -> Array U DIM1 Double
+dwtWorker cs !angles !signal = go layers signal
     where
       go  1 !sig      = doLayer sig 1
-      go !n !sig      = go (n-1) (forceS . csr $ doLayer sig n)
+      go !n !sig      = go (n-1) (forceS . cs $ doLayer sig n)
       {-# INLINE doLayer #-}
       doLayer !sig !n = forceP . lattice (weights ! (Z :. (layers - n))) $ sig
       {-# INLINE weights #-}
@@ -43,9 +43,9 @@ idwt !angles !signal = go layers signal
 
 {-# INLINE lattice #-}
 lattice :: (Source r Double) 
-           => (Double, Double) 
-           -> Array r DIM1 Double
-           -> Array D DIM1 Double
+        => (Double, Double) 
+        -> Array r DIM1 Double
+        -> Array D DIM1 Double
 lattice (!s, !c) !xs = fromPairs . R.map baseOp . toPairs $ xs
     where
       baseOp (!x1, !x2) = (x1 * c + x2 * s,  x1 * s - x2 * c )
@@ -53,8 +53,8 @@ lattice (!s, !c) !xs = fromPairs . R.map baseOp . toPairs $ xs
 
 {-# INLINE toPairs #-}
 toPairs :: (Source r Double) 
-           => Array r DIM1 Double 
-           -> Array D DIM1 (Double, Double)
+        => Array r DIM1 Double 
+        -> Array D DIM1 (Double, Double)
 toPairs !xs = traverse xs twiceShorter wrapPairs
     where
       twiceShorter (Z :. s) = Z :. s `div` 2
@@ -63,8 +63,8 @@ toPairs !xs = traverse xs twiceShorter wrapPairs
 
 {-# INLINE fromPairs #-}
 fromPairs :: (Source r (Double,Double))
-             => Array r DIM1 (Double, Double)
-             -> Array D DIM1 Double
+          => Array r DIM1 (Double, Double)
+          -> Array D DIM1 Double
 fromPairs !xs = traverse xs twiceLonger unwrapPairs
     where
       twiceLonger   (Z :. s) = Z :. 2 * s
@@ -81,9 +81,9 @@ a2w = R.map (sin &&& cos)
 
 
 {-# INLINE inv #-}
-inv :: (Source r Double) 
-       => Array r DIM1 Double 
-       -> Array D DIM1 Double
+inv :: (Source r Double)
+    =>  Array r DIM1 Double 
+    -> Array D DIM1 Double
 inv !xs = backpermute ext reversedIndex xs
     where
       reversedIndex (Z :. i) = Z :. (sh - i - 1)
@@ -92,9 +92,8 @@ inv !xs = backpermute ext reversedIndex xs
 
 
 {-# INLINE csl #-}
-csl :: (Source r Double) 
-       => Array r DIM1 Double 
-       -> Array D DIM1 Double
+csl :: Array U DIM1 Double 
+    -> Array D DIM1 Double
 csl !xs = backpermute ext shift xs
     where
       shift (Z :. i) = if i /= (sh - 1) then Z :. (i + 1) else Z :. 0
@@ -103,9 +102,8 @@ csl !xs = backpermute ext shift xs
 
 
 {-# INLINE csr #-}
-csr :: (Source r Double) 
-       => Array r DIM1 Double 
-       -> Array D DIM1 Double
+csr :: Array U DIM1 Double 
+    -> Array D DIM1 Double
 csr xs = backpermute ext shift xs
     where
       shift (Z :. 0) = Z :. (sh-1)
