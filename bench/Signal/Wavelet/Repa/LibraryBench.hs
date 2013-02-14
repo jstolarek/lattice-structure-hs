83fcaa23043dc0c612bfaed3bf874838765379b2
Jan Stolarek <jan.stolarek@p.lodz.pl>
2013-02-14 17:13:13 +0100

    Fix warnings

--- bench/Signal/Wavelet/Repa/LibraryBench.hs
+++ bench/Signal/Wavelet/Repa/LibraryBench.hs
@@ -1,7 +1,6 @@
 module Signal.Wavelet.Repa.LibraryBench where
 
 import Data.Array.Repa as R
-import System.IO.Unsafe
 
 
 {-# INLINE benchComputeS #-}
@@ -48,13 +47,13 @@ dataExtract (ls, sig) = (Z :. 0, Z :. chunkSize, f sig)
 
 
 {-# INLINE benchAppendS #-}
-benchAppendS :: (Array U DIM1 Double, Array U DIM1 Double) 
+benchAppendS :: (Array U DIM1 Double, Array U DIM1 Double)
              -> Array U DIM1 Double
 benchAppendS (xs, ys) = computeS . append xs $ ys
 
 
 {-# INLINE benchAppendP #-}
-benchAppendP :: (Array U DIM1 Double, Array U DIM1 Double) 
+benchAppendP :: (Array U DIM1 Double, Array U DIM1 Double)
              -> IO (Array U DIM1 Double)
 benchAppendP (xs, ys) = computeP . append xs $ ys
 
@@ -67,7 +66,7 @@ dataAppend (_, sig) = (xs, ys)
 
 
 {-# INLINE benchBckpermS #-}
-benchBckpermS :: (DIM1, DIM1 -> DIM1, Array U DIM1 Double) 
+benchBckpermS :: (DIM1, DIM1 -> DIM1, Array U DIM1 Double)
               -> Array U DIM1 Double
 benchBckpermS (sh, ext, xs) = computeS . backpermute sh ext $ xs
 
@@ -78,7 +77,7 @@ benchBckpermP :: (DIM1, DIM1 -> DIM1, Array U DIM1 Double)
 benchBckpermP (sh, ext, xs) = computeP . backpermute sh ext $ xs
 
 
-dataBckperm :: ([Double], [Double]) 
+dataBckperm :: ([Double], [Double])
             -> (DIM1, DIM1 -> DIM1, Array U DIM1 Double)
 dataBckperm (_, sig) = (Z :. len, id, arr)
     where arr = f sig
@@ -88,13 +87,13 @@ dataBckperm (_, sig) = (Z :. len, id, arr)
 {-# INLINE benchMapS #-}
 benchMapS :: (Double -> Double, Array U DIM1 Double)
           -> Array U DIM1 Double
-benchMapS (f, xs) = computeS . R.map f $ xs
+benchMapS (g, xs) = computeS . R.map g $ xs
 
 
 {-# INLINE benchMapP #-}
 benchMapP :: (Double -> Double, Array U DIM1 Double)
           -> IO (Array U DIM1 Double)
-benchMapP (f, xs) = computeP . R.map f $ xs
+benchMapP (g, xs) = computeP . R.map g $ xs
 
 
 dataMap :: ([Double], [Double]) -> (Double -> Double, Array U DIM1 Double)
@@ -102,22 +101,22 @@ dataMap xs = (id, f . snd $ xs)
 
 
 {-# INLINE benchTraverseS #-}
-benchTraverseS :: (Array U DIM1 Double, DIM1 -> DIM1, 
-                   (DIM1 -> Double) -> DIM1 -> Double) 
+benchTraverseS :: (Array U DIM1 Double, DIM1 -> DIM1,
+                   (DIM1 -> Double) -> DIM1 -> Double)
                -> Array U DIM1 Double
 benchTraverseS (arr, ext, g) = computeS . traverse arr ext $ g
 
 
 {-# INLINE benchTraverseP #-}
-benchTraverseP :: (Array U DIM1 Double, DIM1 -> DIM1, 
+benchTraverseP :: (Array U DIM1 Double, DIM1 -> DIM1,
                    (DIM1 -> Double) -> DIM1 -> Double)
                -> IO (Array U DIM1 Double)
 benchTraverseP (arr, ext, g) = computeP . traverse arr ext $ g
 
 
-dataTraverse :: ([Double], [Double]) 
-             -> (Array U DIM1 Double, DIM1 -> DIM1, 
-                 (DIM1 -> Double) -> DIM1 -> Double) 
+dataTraverse :: ([Double], [Double])
+             -> (Array U DIM1 Double, DIM1 -> DIM1,
+                 (DIM1 -> Double) -> DIM1 -> Double)
 dataTraverse xs = (f . snd $ xs, id, ($))
 
 
